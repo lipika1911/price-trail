056c340b082d1c438eb7b10b994f4f25feb17097
Lipika Arya <lipikaarya1723@gmail.com>
2024-07-23 22:42:08 +0530

    first commit

---
+++ next.config.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ['m.media-amazon.com']
+  }
+}
+
+module.exports = nextConfig
